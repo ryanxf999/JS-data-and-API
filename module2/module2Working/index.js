4fe56d2036354340adf26566d8277865a97aa91d
Ryan Fontenot <ryanxf99@gmail.com>
2022-01-02 08:11:30 -0600

    reset

--- module2/module2Working/index.js
+++
@@ -1,8 +0,0 @@
-//this is just redferencing the express library.
-const express = require ('express');
-const app = express();
-
-//set port to 3000
-app.listen(3000, () => console.log('Listening at 3000'));
-//define location for server to post to localhost
-app.use(express.static('public'));
