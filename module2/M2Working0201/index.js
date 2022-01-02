e6ac7a775f16d588ad669c76ce8848e443ef4f5f
Ryan Fontenot <ryanxf99@gmail.com>
2022-01-02 14:51:31 -0600

    updated

--- module2/M2Working0201/index.js
+++ module2/M2Working0201/index.js
@@ -1,4 +1,5 @@
 //this is just redferencing the express library.
+const { response } = require('express');
 const express = require ('express');
 const app = express();
 
@@ -6,3 +7,10 @@ const app = express();
 app.listen(3000, () => console.log('Listening at 3000'));
 //define location for server to post to localhost
 app.use(express.static('public'));
+
+//setting up a post request with the lat/long of the users location
+app.post('/api', (request, response) =>{
+
+    console.log(request.body);
+
+});
